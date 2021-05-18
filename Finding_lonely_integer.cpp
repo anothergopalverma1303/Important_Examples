c5dd282d33d7244df75f7c65f2ff586d0a986fb2
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2021-05-18 17:38:29 +0530

    Update Finding_lonely_integer.cpp

--- Finding_lonely_integer.cpp
+++ Finding_lonely_integer.cpp
@@ -8,7 +8,10 @@ int findSingle(int ar[], int ar_size)
         // Do XOR of all elements and return
         int res = ar[0];
         for (int i = 1; i < ar_size; i++)
-            res = res ^ ar[i];
+            res = res ^ ar[i];                        //here the philosophy of usage of ^ is that it act as  a switch ie if same sinal are applied
+                                                      //then it is gonna be off (0^0 = 0, 1^1 = 0) and if different signals are applied it became on
+                                                      // (1^0 = 1, 0^1 = 0). So the repeating signals repeated themselves and closed themselves but the 
+                                                      // single integer were not able to switch themself off.
 
         return res;
     }
