39e5cfb1729925982949e6eb5c5ed2b9d1f14b0e
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2021-05-18 18:03:52 +0530

    Update Finding_lonely_integer.cpp

--- Finding_lonely_integer.cpp
+++ Finding_lonely_integer.cpp
@@ -6,14 +6,20 @@ using namespace std;
 int findSingle(int ar[], int ar_size)
     {
         // Do XOR of all elements and return
+    if(n = 1)
+        return (arr[0]);
+    else
+    {
         int res = ar[0];
         for (int i = 1; i < ar_size; i++)
-            res = res ^ ar[i];                        //here the philosophy of usage of ^ is that it act as  a switch ie if same sinal are applied
-                                                      //then it is gonna be off (0^0 = 0, 1^1 = 0) and if different signals are applied it became on
+            res = res ^ ar[i];  
+        return res                                    //here the philosophy of usage of ^ is that it act as  a switch ie if same sinal are applied
+    }                                                 //then it is gonna be off (0^0 = 0, 1^1 = 0) and if different signals are applied it became on
                                                       // (1^0 = 1, 0^1 = 0). So the repeating signals repeated themselves and closed themselves but the 
                                                       // single integer were not able to switch themself off.
+                                                      //* but it can only work when the repeating characters are only twice in number.
 
-        return res;
+        
     }
 
 // Driver code
