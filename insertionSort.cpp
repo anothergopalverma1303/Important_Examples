899dd31e789deb9c477fa01a7603e5dda9990a58
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2021-10-11 14:52:52 +0530

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -8,18 +8,18 @@ void insertionSort(int arr[], int n)
 	int i, key, j;
 	for (i = 1; i < n; i++)
 	{
-		key = arr[i];
+		key = arr[i]; //SELECT THE FRIST UNSORTED ELEMENT`	
 		j = i - 1;
 
 		/* Move elements of arr[0..i-1], that are
 		greater than key, to one position ahead
 		of their current position */
-		while (j >= 0 && arr[j] > key)
+		while (j >= 0 && arr[j] > key) //THIS LOOP SHIFT ALL THE ELEMENT TO THE RIGHT
 		{
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
-		arr[j + 1] = key;
+		arr[j + 1] = key; //IT INSERT THE UNSORTED ELEMENT IN THE SPACE CREATED
 	}
 }
 
