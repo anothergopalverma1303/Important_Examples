0e29452a2e0ca4dfd7384d0cc8a6572ddc5848b1
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-04-15 15:54:23 +0530

    Create countPermutations.java

---
+++ countPermutations.java
@@ -0,0 +1,23 @@
+package SkipCharacter;
+
+import java.util.Scanner;
+
+public class Skip {
+    public static void main(String[] args) {
+        System.out.println(permutations("", "GOPAL"));
+    }
+    static int permutations(String p, String up) {
+        if(up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for(int i = 0 ; i <= p.length() ; i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count = count + permutations(f + ch + s, up.substring(1));
+        }
+        return count;
+    }
+}
+
