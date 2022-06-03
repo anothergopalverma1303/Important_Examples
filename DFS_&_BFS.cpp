2ddb4e3c5ed33fa4d62dfee1ab7adcc5485f8a6d
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-06-03 23:22:01 +0530

    Update DFS_&_BFS.cpp

--- DFS_&_BFS.cpp
+++ DFS_&_BFS.cpp
@@ -102,32 +102,21 @@ void Graph::addEdge(int v, int w)
 
 void Graph::BFS(int s)
 {
-	// Mark all the vertices as not visited
-	vector<bool> visited;
-	visited.resize(V,false);
-
-	// Create a queue for BFS
 	list<int> queue;
 
-	// Mark the current node as visited and enqueue it
-	visited[s] = true;
+	vis[s] = true;
 	queue.push_back(s);
 
 	while(!queue.empty())
 	{
-		// Dequeue a vertex from queue and print it
 		s = queue.front();
-		cout << s << " ";
+		cout<<s<<" ";
 		queue.pop_front();
-
-		// Get all adjacent vertices of the dequeued
-		// vertex s. If a adjacent has not been visited,
-		// then mark it visited and enqueue it
-		for (auto adjecent: adj[s])
+		for(auto adjecent:adj[s])
 		{
-			if (!visited[adjecent])
+			if(!vis[adjecent])
 			{
-				visited[adjecent] = true;
+				vis[adjecent] = true;
 				queue.push_back(adjecent);
 			}
 		}
